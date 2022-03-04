3a2084e61b346d53cd5bdb6b71efc319115610f7
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-03-04 15:53:28 +0600

    dirst

---
+++ alert.js
@@ -0,0 +1,16 @@
+console.log('I am here');
+
+const maComing = () => {
+    alert('Ammu is Coming');
+}
+const askPicnic = () => {
+    const response = confirm('Are You Going To Picnic')
+    console.log(response);
+}
+const askName = () => {
+    const name = prompt('What is your name');
+    if(name) {
+        console.log(name);
+    }
+}
+// alert('Ma is coming')
